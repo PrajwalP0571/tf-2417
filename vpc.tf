a244459639119ceefd5053bc6b48fb1c5e2dab32
ravi2krishna <ravi2krishna@gmail.com>
2024-05-24 10:45:58 +0530

    create public route table

--- vpc.tf
+++ vpc.tf
@@ -38,3 +38,17 @@ resource "aws_internet_gateway" "ibm-igw" {
     Name = "ibm internet"
   }
 }
+
+# Create Public Routing
+resource "aws_route_table" "ibm-pub-rt" {
+  vpc_id = aws_vpc.ibm-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ibm-igw.id
+  }
+
+  tags = {
+    Name = "ibm public route table"
+  }
+}
