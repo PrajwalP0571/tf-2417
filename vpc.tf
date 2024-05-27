d6fe9217ec3a86348c49f106a4e13d14380d8320
ravi2krishna <ravi2krishna@gmail.com>
2024-05-27 10:16:34 +0530

    create private nacl

--- vpc.tf
+++ vpc.tf
@@ -102,3 +102,29 @@ resource "aws_network_acl" "ibm-pub-nacl" {
   }
 }
 
+# Create Private NACL
+resource "aws_network_acl" "ibm-pvt-nacl" {
+  vpc_id = aws_vpc.ibm-vpc.id
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  tags = {
+    Name = "ibm-private-nacl"
+  }
+}
