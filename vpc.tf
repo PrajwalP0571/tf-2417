81d85fca8959780598a84302a75b216aad325ce5
ravi2krishna <ravi2krishna@gmail.com>
2024-05-27 10:34:33 +0530

    fix unique resouce

--- vpc.tf
+++ vpc.tf
@@ -207,7 +207,7 @@ resource "aws_vpc_security_group_ingress_rule" "ibm-pvt-pg" {
 }
 
 # Security Group Out Bound Rule
-resource "aws_vpc_security_group_egress_rule" "ibm-all-traffic" {
+resource "aws_vpc_security_group_egress_rule" "ibm-all-traffic-pvt" {
   security_group_id = aws_security_group.ibm-pub-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
